d29e75a63b7184b3cde73cc6296fee7d133e8413
Javier A. Roque Sañudo <javicaib.mail@gmail.com>
2024-01-04 20:35:01 +0100

    check is valid token-user

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,11 +2,12 @@ import { NextFunction, Request, Response } from "express";
 import type { t } from 'i18next';
 import jwt from "jsonwebtoken";
 import { getSecretWord } from "../helpers/token";
+import User from "../models/user.model";
 export interface CustomRequest extends Request {
   t: typeof t;
   decode?: any; // Define la propiedad userData opcional
 }
-const check_auth = (req: CustomRequest, res: Response, next: NextFunction) => {
+const check_auth = async (req: CustomRequest, res: Response, next: NextFunction) => {
   let token;
   if (
     req.headers.authorization &&
@@ -15,10 +16,19 @@ const check_auth = (req: CustomRequest, res: Response, next: NextFunction) => {
     try {
       token = req.headers.authorization.split(" ")[1];
       const decode = jwt.verify(token, getSecretWord());
-      req.decode = decode
+      if (typeof decode !== 'string') {
+        const user = await User.findById(decode.id)
+        if (!user) {
+          return res.status(401).json({ msg: req.t("invalid-token") });
+        }
+        req.decode = decode
 
-      return next();
+        return next()
+      }
+
+      return res.status(401).json({ msg: req.t("invalid-token") });
     } catch (error) {
+      console.log(error)
       return res.status(401).json({ msg: req.t("invalid-token") });
     }
   }
